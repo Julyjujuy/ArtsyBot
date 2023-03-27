175399c69db13e5a67e4c743f5dbaedfd1d98928
Julian Alonso <tantissimo.entusiasmo@gmail.com>
2023-03-27 20:04:57 +0200

    Add project files.

---
+++ ArtsyBot/LogicMethods.cs
@@ -0,0 +1,44 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.XPath;
+
+
+namespace ArtsyBot
+{
+    internal class LogicMethods
+    {
+        //takes the url and transform the page into a HtmlDocument
+        public static HtmlDocument GetHtmlDocument(string url)
+        {
+            HtmlWeb web = new HtmlWeb();
+            return web.Load(url);
+        }
+        public static void ScrapeWebsite(string url)
+        {
+            // Load the website's HTML document
+            HtmlDocument htmlDoc = GetHtmlDocument(url);
+
+            // Find the elements to scrape using XPath
+            // Element's names are temporary
+            HtmlNode imageElement = htmlDoc.DocumentNode.SelectSingleNode("//img[@class='my-image']");
+            HtmlNode DescriptionElement = htmlDoc.DocumentNode.SelectSingleNode("//span[@class='y']");
+            HtmlNode urlElement = htmlDoc.DocumentNode.SelectSingleNode("//a[@href='/z']");
+
+            // Extract the values of the elements as strings
+            string imageUrl = imageElement?.GetAttributeValue("image", "").Trim();
+            string yValue = DescriptionElement?.GetAttributeValue("description", "").Trim();
+            string zValue = urlElement?.GetAttributeValue("link", "").Trim();
+
+            // Do something with the scraped data (store in sql)
+            Console.WriteLine($"URL of image: {imageUrl}");
+            Console.WriteLine($"Value of Y: {yValue}");
+            Console.WriteLine($"Value of Z: {zValue}");
+        }
+
+
+    }
+}
---
+++ ArtsyBot/Program.cs
@@ -0,0 +1,26 @@
+﻿using HtmlAgilityPack;
+using System;
+
+namespace ArtsyBot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let us scrap the whole internet!");
+            string url = "https://www.example.com";
+
+            // Call the GetHtmlDocument method to load the website's HTML document
+            HtmlDocument htmlDoc = LogicMethods.GetHtmlDocument(url);
+
+            // Use the HtmlDocument object to extract data from the website
+            // Names' of the Nodes are temporary
+            // foreach (HtmlNode node in htmlDoc.DocumentNode.SelectNodes("//div[@class='example-class']"))
+            // {
+                // Do something with the data here
+                // Save the data into SQL Database
+            // }
+
+        }
+    }
+}
