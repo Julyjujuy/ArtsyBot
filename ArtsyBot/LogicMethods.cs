bbc41ed7a616866cb99bf70ccbe5b70c5cd32665
Julian Alonso <tantissimo.entusiasmo@gmail.com>
2023-03-28 00:57:53 +0200

    Work in progress

--- ArtsyBot/LogicMethods.cs
+++ ArtsyBot/LogicMethods.cs
@@ -1,10 +1,5 @@
 ﻿using HtmlAgilityPack;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.XPath;
+
 
 
 namespace ArtsyBot
