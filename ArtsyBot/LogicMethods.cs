c897024c39dbff054fb469b5792a11aba6f74642
Julian Alonso <tantissimo.entusiasmo@gmail.com>
2023-04-19 16:19:19 +0200

    Added scrape loop with 1 for loop with 3 nested foreach loops. Added method to find out the number of pages on the website.

--- ArtsyBot/LogicMethods.cs
+++ ArtsyBot/LogicMethods.cs
@@ -4,7 +4,7 @@
 
 namespace ArtsyBot
 {
-    internal class LogicMethods
+    public class LogicMethods
     {
         //takes the url and transform the page into a HtmlDocument
         public static HtmlDocument GetHtmlDocument(string url)
@@ -12,28 +12,26 @@ namespace ArtsyBot
             HtmlWeb web = new HtmlWeb();
             return web.Load(url);
         }
-        public static void ScrapeWebsite(string url)
+       public static void ScrapeWebsite(string url)
         {
             // Load the website's HTML document
             HtmlDocument htmlDoc = GetHtmlDocument(url);
 
             // Find the elements to scrape using XPath
-            // Element's names are temporary
-            HtmlNode imageElement = htmlDoc.DocumentNode.SelectSingleNode("//img[@class='my-image']");
-            HtmlNode DescriptionElement = htmlDoc.DocumentNode.SelectSingleNode("//span[@class='y']");
-            HtmlNode urlElement = htmlDoc.DocumentNode.SelectSingleNode("//a[@href='/z']");
+            HtmlNode imageElement = htmlDoc.DocumentNode.SelectSingleNode("//img[@class='ImageRow__StyledImageWithFallback-sc-1s4yel2-3 jDatHc no-js-150613881_1_x']");
+            HtmlNode descriptionElement = htmlDoc.DocumentNode.SelectSingleNode("//div[@class='sc-iAEyYk DescriptionSection__StyledBody-sc-trkwix-2 kiRtmS gtFpYt']");
+            HtmlNode urlElement = htmlDoc.DocumentNode.SelectSingleNode("//a[@class='no-js-150613881_1_x']");
 
             // Extract the values of the elements as strings
             string imageUrl = imageElement?.GetAttributeValue("src", "").Trim();
-            string yValue = DescriptionElement?.GetAttributeValue("description", "").Trim();
-            string zValue = urlElement?.GetAttributeValue("href", "").Trim();
+            string description = descriptionElement?.InnerText.Trim();
+            string linkPageObject = urlElement?.GetAttributeValue("href", "").Trim();
 
             // Do something with the scraped data 
             Console.WriteLine($"URL of image: {imageUrl}");
-            Console.WriteLine($"Value of Y: {yValue}");
-            Console.WriteLine($"Value of Z: {zValue}");
+            Console.WriteLine($"Description: {description}");
+            Console.WriteLine($"Link to page of the object: {linkPageObject}");
         }
-
 
     }
 }
--- ArtsyBot/Program.cs
+++ ArtsyBot/Program.cs
@@ -5,16 +5,67 @@ namespace ArtsyBot
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private const string Path = @"C:\sample.txt";
+
+        public static void Main()
         {
-            Console.WriteLine("Let us scrap the whole internet!");
-            string url = "https://www.example.com";
+            string baseUrl = "https://www.liveauctioneers.com/c/clocks/74/";
+            string Path = "C:\\temp\\htmlfile.html";
 
-            // Call the GetHtmlDocument method to load the website's HTML document
-            HtmlDocument htmlDoc = LogicMethods.GetHtmlDocument(url);
+            // Load the first page of the website
+            HtmlDocument htmlDoc = LogicMethods.GetHtmlDocument(baseUrl);
+            htmlDoc.Save(Path);
 
-            
+            // Find the total number of pages on the website
+            int totalPages = 0;
+            HtmlNode pageNode = htmlDoc.DocumentNode.SelectSingleNode("//a[@title='Last Page']");
+            if (pageNode != null)
+            {
+                string hrefValue = pageNode.GetAttributeValue("href", "");
+                if (!string.IsNullOrEmpty(hrefValue))
+                {
+                    int startIndex = hrefValue.LastIndexOf("page=") + "page=".Length;
+                    int endIndex = hrefValue.LastIndexOf("&", StringComparison.InvariantCulture);
+                    if (startIndex >= 0 && endIndex >= 0 && endIndex > startIndex)
+                    {
+                        int.TryParse(hrefValue.Substring(startIndex, endIndex - startIndex), out totalPages);
+                    }
+                }
+            }
 
+            // Scrape the data from each page on the website
+            for (int i = 2; i <= totalPages; i++)
+            {
+                // Load the current page of the website
+                string currentUrl = $"{baseUrl}?page={i}";
+                htmlDoc = LogicMethods.GetHtmlDocument(currentUrl);
+                htmlDoc.Save(Path);
+
+                // Find the elements to scrape using XPath
+                HtmlNodeCollection imageElements = htmlDoc.DocumentNode.SelectNodes("//img[@class='ImageRow__StyledImageWithFallback-sc-1s4yel2-3 jDatHc no-js-150613881_1_x']");
+                HtmlNodeCollection DescriptionElements = htmlDoc.DocumentNode.SelectNodes("//div[@class='sc-iAEyYk DescriptionSection__StyledBody-sc-trkwix-2 kiRtmS gtFpYt']");
+                HtmlNodeCollection urlElements = htmlDoc.DocumentNode.SelectNodes("//a[@class='no-js-150613881_1_x']");
+
+                // Extract the values of the elements as strings
+                foreach (HtmlNode imageElement in imageElements)
+                {
+                    string imageUrl = imageElement?.GetAttributeValue("src", "").Trim();
+                    Console.WriteLine($"URL of image: {imageUrl}");
+                }
+
+                foreach (HtmlNode DescriptionElement in DescriptionElements)
+                {
+                    string description = DescriptionElement?.InnerText.Trim();
+                    Console.WriteLine($"Value of Description: {description}");
+                }
+
+                foreach (HtmlNode urlElement in urlElements)
+                {
+                    string linkPageObject = urlElement?.GetAttributeValue("href", "").Trim();
+                    Console.WriteLine($"Value of URL: {linkPageObject}");
+                }
+            }
         }
+
     }
 }
