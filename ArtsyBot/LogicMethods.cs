484898b499032a4ed60ab7d6f6b825c2c70fcac5
Julyjujuy <tantissimo.entusiasmo@gmail.com>
2023-05-01 13:47:23 +0200

    changed to a more simpler method build, still not finish. now scrapewebsite takes a baseUrl argument instead of a htmldocument, solved issue. now it tries to go into every link on the page before selecting single nodes. but still get 0 links.

--- ArtsyBot/LogicMethods.cs
+++ ArtsyBot/LogicMethods.cs
@@ -12,26 +12,56 @@ namespace ArtsyBot
             HtmlWeb web = new HtmlWeb();
             return web.Load(url);
         }
-       public static void ScrapeWebsite(string url)
+
+        // Find the total number of pages on the website
+        public static int GetTotalPages(HtmlDocument htmlDoc)
+        {
+            int totalPages = 0;
+            HtmlNode pageNode = htmlDoc.DocumentNode.SelectSingleNode("//a[@title='Last Page']");
+            if (pageNode != null)
+            {
+                string hrefValue = pageNode.GetAttributeValue("href", "");
+                if (!string.IsNullOrEmpty(hrefValue))
+                {
+                    int startIndex = hrefValue.LastIndexOf("page=") + "page=".Length;
+                    int endIndex = hrefValue.LastIndexOf("&", StringComparison.InvariantCulture);
+                    if (startIndex >= 0 && endIndex >= 0 && endIndex > startIndex)
+                    {
+                        int.TryParse(hrefValue.Substring(startIndex, endIndex - startIndex), out totalPages);
+                    }
+                }
+            }
+
+            return totalPages;
+        }
+
+        public static void ScrapeWebsite(string url)
         {
             // Load the website's HTML document
             HtmlDocument htmlDoc = GetHtmlDocument(url);
 
             // Find the elements to scrape using XPath
-            HtmlNode imageElement = htmlDoc.DocumentNode.SelectSingleNode("//img[@class='ImageRow__StyledImageWithFallback-sc-1s4yel2-3 jDatHc no-js-150613881_1_x']");
-            HtmlNode descriptionElement = htmlDoc.DocumentNode.SelectSingleNode("//div[@class='sc-iAEyYk DescriptionSection__StyledBody-sc-trkwix-2 kiRtmS gtFpYt']");
-            HtmlNode urlElement = htmlDoc.DocumentNode.SelectSingleNode("//a[@class='no-js-150613881_1_x']");
+            HtmlNodeCollection imageElements = htmlDoc.DocumentNode.SelectNodes("//img[@class='ImageWithHover__PrimaryImageWithFallback-sc-1b860cz-1 jYPLtE no-js-150133299_1_x']");
+            HtmlNodeCollection descriptionElements = htmlDoc.DocumentNode.SelectNodes("//div[@class='sc-iAEyYk DescriptionSection__StyledBody-sc-trkwix-2 kiRtmS gtFpYt']");
+            HtmlNodeCollection urlElements = htmlDoc.DocumentNode.SelectNodes("//a[@class='no-js-150613881_1_x']");
 
             // Extract the values of the elements as strings
-            string imageUrl = imageElement?.GetAttributeValue("src", "").Trim();
-            string description = descriptionElement?.InnerText.Trim();
-            string linkPageObject = urlElement?.GetAttributeValue("href", "").Trim();
+            if (imageElements != null && descriptionElements != null && urlElements != null &&
+                imageElements.Count == descriptionElements.Count && descriptionElements.Count == urlElements.Count)
+            {
+                for (int i = 0; i < imageElements.Count; i++)
+                {
+                    string? imageUrl = imageElements[i]?.GetAttributeValue("src", "").Trim();
+                    string? description = descriptionElements[i]?.InnerText.Trim();
+                    string? linkPageObject = urlElements[i]?.GetAttributeValue("href", "").Trim();
 
-            // Do something with the scraped data 
-            Console.WriteLine($"URL of image: {imageUrl}");
-            Console.WriteLine($"Description: {description}");
-            Console.WriteLine($"Link to page of the object: {linkPageObject}");
+                    // Do something with the scraped data 
+                    Console.WriteLine($"URL of image: {imageUrl}");
+                    Console.WriteLine($"Description: {description}");
+                    Console.WriteLine($"Link to page of the object: {linkPageObject}");
+                }
+            }
         }
-
     }
 }
+
--- ArtsyBot/Program.cs
+++ ArtsyBot/Program.cs
@@ -5,67 +5,35 @@ namespace ArtsyBot
 {
     internal class Program
     {
-        private const string Path = @"C:\sample.txt";
 
         public static void Main()
         {
             string baseUrl = "https://www.liveauctioneers.com/c/clocks/74/";
-            string Path = "C:\\temp\\htmlfile.html";
 
             // Load the first page of the website
             HtmlDocument htmlDoc = LogicMethods.GetHtmlDocument(baseUrl);
-            htmlDoc.Save(Path);
 
             // Find the total number of pages on the website
-            int totalPages = 0;
-            HtmlNode pageNode = htmlDoc.DocumentNode.SelectSingleNode("//a[@title='Last Page']");
-            if (pageNode != null)
+            int totalPages = LogicMethods.GetTotalPages(htmlDoc);
+            Console.WriteLine(totalPages.ToString());
+
+            // Scrape data from all pages of the website
+            for (int page = 1; page <= totalPages; page++)
             {
-                string hrefValue = pageNode.GetAttributeValue("href", "");
-                if (!string.IsNullOrEmpty(hrefValue))
-                {
-                    int startIndex = hrefValue.LastIndexOf("page=") + "page=".Length;
-                    int endIndex = hrefValue.LastIndexOf("&", StringComparison.InvariantCulture);
-                    if (startIndex >= 0 && endIndex >= 0 && endIndex > startIndex)
-                    {
-                        int.TryParse(hrefValue.Substring(startIndex, endIndex - startIndex), out totalPages);
-                    }
-                }
+                string url = $"{baseUrl}?page={page}";
+                LogicMethods.ScrapeWebsite(url);
             }
 
-            // Scrape the data from each page on the website
-            for (int i = 2; i <= totalPages; i++)
+
+            // Scrape the first page
+            LogicMethods.ScrapeWebsite(baseUrl);
+
+            // Scrape the remaining pages
+            for (int page = 2; page <= totalPages; page++)
             {
-                // Load the current page of the website
-                string currentUrl = $"{baseUrl}?page={i}";
-                htmlDoc = LogicMethods.GetHtmlDocument(currentUrl);
-                htmlDoc.Save(Path);
-
-                // Find the elements to scrape using XPath
-                HtmlNodeCollection imageElements = htmlDoc.DocumentNode.SelectNodes("//img[@class='ImageRow__StyledImageWithFallback-sc-1s4yel2-3 jDatHc no-js-150613881_1_x']");
-                HtmlNodeCollection DescriptionElements = htmlDoc.DocumentNode.SelectNodes("//div[@class='sc-iAEyYk DescriptionSection__StyledBody-sc-trkwix-2 kiRtmS gtFpYt']");
-                HtmlNodeCollection urlElements = htmlDoc.DocumentNode.SelectNodes("//a[@class='no-js-150613881_1_x']");
-
-                // Extract the values of the elements as strings
-                foreach (HtmlNode imageElement in imageElements)
-                {
-                    string imageUrl = imageElement?.GetAttributeValue("src", "").Trim();
-                    Console.WriteLine($"URL of image: {imageUrl}");
-                }
-
-                foreach (HtmlNode DescriptionElement in DescriptionElements)
-                {
-                    string description = DescriptionElement?.InnerText.Trim();
-                    Console.WriteLine($"Value of Description: {description}");
-                }
-
-                foreach (HtmlNode urlElement in urlElements)
-                {
-                    string linkPageObject = urlElement?.GetAttributeValue("href", "").Trim();
-                    Console.WriteLine($"Value of URL: {linkPageObject}");
-                }
+                string pageUrl = $"{baseUrl}?page={page}";
+                LogicMethods.ScrapeWebsite(pageUrl);
             }
         }
-
     }
 }
