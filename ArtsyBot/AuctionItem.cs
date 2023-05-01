2e9f11f4687724c84b90702d0746913a72cda4d3
Julyjujuy <tantissimo.entusiasmo@gmail.com>
2023-05-01 14:42:46 +0200

    changed back to working build

---
+++ ArtsyBot/AuctionItem.cs
@@ -0,0 +1,9 @@
+﻿namespace ArtsyBot
+{
+    internal class AuctionItem
+    {
+        public string ImageUrl { get; set; }
+        public string Description { get; set; }
+        public string PageUrl { get; set; }
+    }
+}
